1e6298499094608815252e877d5d505e63dce3ce
Arthemus <arthemus.moreira@gmail.com>
2019-06-25 12:11:25 +1000

    Define dynamic port.

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
+import { PORT } from './config';
+
 import App from './App';
+
 import UserController from './users/user.controller';
 import AuthenticationController from './authentication/authentication.controller';
- 
-const app = new App([
-    new UserController(),
-    new AuthenticationController()
-  ],
-  3001,
+
+const app = new App(
+  [new UserController(), new AuthenticationController()],
+  PORT,
   '0.0.0.0'
 );
- 
+
 app.listen();
