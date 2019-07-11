e044e261e763e2579cf1a86b0c80f2ab15a13899
Arthemus <arthemus.moreira@gmail.com>
2019-07-11 11:06:52 +1000

    Fixing Role model export.

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,7 @@
 import { User } from './user.model';
+import { Role } from './role.model';
 import { UserLegancy } from './userLegancy.model';
 
-export { User, UserLegancy };
+export { User, Role, UserLegancy };
 
 export const arrayOfModels = [UserLegancy];
