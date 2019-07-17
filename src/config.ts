1ef65eee91e0e123031092a00219a8ab1fa1d278
Lucas <csalucasnascimento@gmail.com>
2019-07-17 12:43:47 +1000

    removing S3 depedency

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 export const DEBUG = process.env.DEBUG ? Boolean(process.env.DEBUG) : false;
@@ -10,8 +10,3 @@ export const dbSchema = process.env.DATABASE_SCHEMA;
 export const dbUsername = process.env.DATABASE_USERNAME;
 export const dbPassword = process.env.DATABASE_PASSWORD;
 export const dbHost = process.env.DATABASE_HOST;
-
-// S3 Bucket
-export const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
-export const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-export const bucket = process.env.AWS_BUCKET;
