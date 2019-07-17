d7456bbfacf9550cccf5a8d2ccd660f5d5e0f752
Lucas <csalucasnascimento@gmail.com>
2019-07-17 10:40:12 +1000

    adding health controller

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
-import { PORT } from './config';
+import { PORT } from "./config";
 
-import App from './App';
+import App from "./App";
 
-import UserController from './controllers/users/user.controller';
-import UserLegancyController from './controllers/users/userLegancy.controller';
-import AuthenticationController from './controllers/authentication/authentication.controller';
+import AuthenticationController from "./controllers/authentication/authentication.controller";
+import HealthController from "./controllers/health/health.controller";
+import UserController from "./controllers/users/user.controller";
+import UserLegancyController from "./controllers/users/userLegancy.controller";
 
 const app = new App(
   [
+    new AuthenticationController(),
+    new HealthController(),
     new UserController(),
-    new UserLegancyController(),
-    new AuthenticationController()
+    new UserLegancyController()
   ],
   PORT,
-  '0.0.0.0'
+  "0.0.0.0"
 );
 
 app.listen();
