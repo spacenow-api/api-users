fda256d5855f93eddd3025f47afc4090924dbc1f
Lucas <csalucasnascimento@gmail.com>
2019-08-01 12:22:58 +1000

    delete with auth

--- src/controllers/users/user.controller.ts
+++ src/controllers/users/user.controller.ts
@@ -2,8 +2,6 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import sequelizeErrorMiddleware from "../../helpers/middlewares/sequelize-error-middleware";
 import authMiddleware from "../../helpers/middlewares/auth-middleware";
-import httpException from "../../helpers/exceptions/HttpException";
-import errorMiddleware from "../../helpers/middlewares/error-middleware";
 
 import { IUser } from "./user.interface";
 
@@ -23,10 +21,6 @@ class UsersController {
     this.router.get(`${this.path}/:id`, this.getUser);
     this.router.post(this.path, this.createUser);
     this.router.patch(this.path, this.createUser);
-    this.router.delete(
-      `${this.path}/deleteByEmail/:email`,
-      this.deleteUserByEmail
-    );
   }
 
   private getAllUsers = async (
@@ -60,25 +54,6 @@ class UsersController {
     try {
       const user = await User.create(data);
       res.send(user);
-    } catch (error) {
-      sequelizeErrorMiddleware(error, req, res, next);
-    }
-  };
-
-  private deleteUserByEmail = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const user = await User.findOne({ where: { email: req.params.email } });
-      if (!user) new httpException(400, "User does not exist!");
-      try {
-        await User.destroy({ where: { email: req.params.email } });
-        new httpException(200, "User deleted successful!");
-      } catch (error) {
-        errorMiddleware(error, req, res, next);
-      }
     } catch (error) {
       sequelizeErrorMiddleware(error, req, res, next);
     }
--- src/controllers/users/userLegancy.controller.ts
+++ src/controllers/users/userLegancy.controller.ts
@@ -1,11 +1,13 @@
 import { Router, Request, Response, NextFunction } from "express";
 
 import sequelizeErrorMiddleware from "../../helpers/middlewares/sequelize-error-middleware";
+import authMiddleware from "../../helpers/middlewares/auth-middleware";
+import httpException from "../../helpers/exceptions/HttpException";
+import errorMiddleware from "../../helpers/middlewares/error-middleware";
 
 import { UserLegancy } from "../../models";
 
 class UserLegancyController {
-
   private router = Router();
 
   constructor() {
@@ -14,12 +16,42 @@ class UserLegancyController {
 
   private intializeRoutes() {
     this.router.get(`/users/legancy/:id`, this.getUserLegancyById);
+    this.router.delete(
+      `/users/legacy/deleteByEmail`,
+      authMiddleware,
+      this.deleteUserByEmail
+    );
   }
 
-  private getUserLegancyById = async (req: Request, res: Response, next: NextFunction) => {
+  private getUserLegancyById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
     try {
       const user = await UserLegancy.findOne({ where: { id: req.params.id } });
       res.send(user);
+    } catch (error) {
+      sequelizeErrorMiddleware(error, req, res, next);
+    }
+  };
+
+  private deleteUserByEmail = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+      const email = req.query.email;
+      const user = await UserLegancy.findOne({ where: { email: email } });
+      if (!user) next(new httpException(400, "User does not exist!"));
+      else
+        try {
+          await UserLegancy.destroy({ where: { id: user.id } });
+          next(new httpException(200, "User deleted successful!"));
+        } catch (error) {
+          errorMiddleware(error, req, res, next);
+        }
     } catch (error) {
       sequelizeErrorMiddleware(error, req, res, next);
     }
