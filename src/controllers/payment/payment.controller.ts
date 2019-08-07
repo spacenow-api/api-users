59cb03495ae082c9d2b9f9bdf436c1a6a79dde46
Arthemus <arthemus.moreira@gmail.com>
2019-08-07 12:26:33 +1000

    Declaring Stripe methods.

--- src/controllers/payment/payment.controller.ts
+++ src/controllers/payment/payment.controller.ts
@@ -1,12 +1,19 @@
 import { Router, Request, Response, NextFunction } from "express";
+import Stripe from "stripe";
 
 import sequelizeErrorMiddleware from "../../helpers/middlewares/sequelize-error-middleware";
 import authMiddleware from "../../helpers/middlewares/auth-middleware";
-import httpException from "../../helpers/exceptions/HttpException";
+import HttpException from "../../helpers/exceptions/HttpException";
+
+import { UserProfileLegancy } from "./../../models";
+
+import { payment } from './../../config';
 
 class PaymentController {
 
   private router = Router();
+
+  private stripeInstance: Stripe = new Stripe(payment.stripe.secretKey);
 
   constructor() {
     this.intializeRoutes();
@@ -19,29 +26,62 @@ class PaymentController {
   }
 
   private getAccount = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      res.end();
-    } catch (err) {
-      console.error(err);
-      sequelizeErrorMiddleware(err, req, res, next);
+    if (req.userIdDecoded) {
+      try {
+        const userProfileObj = await UserProfileLegancy.findOne({ where: { userId: req.userIdDecoded } });
+        if (!userProfileObj) throw new HttpException(400, `User ${req.userIdDecoded} does not have a valid Profile.`);
+        if (!userProfileObj.accountId) throw new HttpException(400, `User ${req.userIdDecoded} does not have Stripe Account ID.`);
+        const account = await this.stripeInstance.accounts.retrieve(userProfileObj.accountId);
+        if (!account) throw new HttpException(400, `Stripe Account ${userProfileObj.accountId} not found.`);
+        res.send(account);
+      } catch (err) {
+        console.error(err);
+        sequelizeErrorMiddleware(err, req, res, next);
+      }
     }
   };
 
   private createAccount = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      res.end();
-    } catch (err) {
-      console.error(err);
-      sequelizeErrorMiddleware(err, req, res, next);
+    if (req.userIdDecoded) {
+      const ts = Math.round(new Date().getTime() / 1000);
+      try {
+        const userProfileObj = await UserProfileLegancy.findOne({ where: { userId: req.userIdDecoded } });
+        if (!userProfileObj) throw new HttpException(400, `User ${req.userIdDecoded} does not have a valid Profile.`);
+        if (userProfileObj.accountId) throw new HttpException(400, `User ${req.userIdDecoded} already has a valid Stripe Account ID.`);
+
+        // Stripe Account Details...
+        const data = req.body;
+        data.tos_acceptance = {
+          date: ts,
+          ip: req.connection.remoteAddress,
+        };
+
+        const accountCreated = await this.stripeInstance.accounts.create(data);
+        if (!accountCreated) throw new HttpException(400, `User ${req.userIdDecoded} does not have a valid Profile.`);
+        await UserProfileLegancy.update({ accountId: accountCreated.id }, { where: { profileId: userProfileObj.profileId } });
+
+        res.send(accountCreated);
+      } catch (err) {
+        console.error(err);
+        sequelizeErrorMiddleware(err, req, res, next);
+      }
     }
   };
 
   private removeAccount = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      res.end();
-    } catch (err) {
-      console.error(err);
-      sequelizeErrorMiddleware(err, req, res, next);
+    if (req.userIdDecoded) {
+      try {
+        const userProfileObj = await UserProfileLegancy.findOne({ where: { userId: req.userIdDecoded } });
+        if (!userProfileObj) throw new HttpException(400, `User ${req.userIdDecoded} does not have a valid Profile.`);
+        if (userProfileObj.accountId) {
+          await this.stripeInstance.accounts.del(userProfileObj.accountId);
+          await UserProfileLegancy.update({ accountId: null }, { where: { profileId: userProfileObj.profileId } });
+        }
+        res.end();
+      } catch (err) {
+        console.error(err);
+        sequelizeErrorMiddleware(err, req, res, next);
+      }
     }
   };
 }
