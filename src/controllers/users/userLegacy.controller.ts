fea9ec6fbb5abfc754550b25e4492a770bf2d827
Lucas Nascimento <csalucasnascimento@gmail.com>
2019-08-12 07:59:51 +1000

    change to update

--- src/controllers/users/userLegacy.controller.ts
+++ src/controllers/users/userLegacy.controller.ts
@@ -1,109 +1,117 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response, NextFunction } from 'express';
 
-import sequelizeErrorMiddleware from "../../helpers/middlewares/sequelize-error-middleware";
-import authMiddleware from "../../helpers/middlewares/auth-middleware";
-import httpException from "../../helpers/exceptions/HttpException";
-import errorMiddleware from "../../helpers/middlewares/error-middleware";
+import sequelizeErrorMiddleware from '../../helpers/middlewares/sequelize-error-middleware';
+import authMiddleware from '../../helpers/middlewares/auth-middleware';
+import httpException from '../../helpers/exceptions/HttpException';
+import errorMiddleware from '../../helpers/middlewares/error-middleware';
 
-import { UserLegacy, UserProfileLegacy } from "../../models";
+import { UserLegacy, UserProfileLegacy } from '../../models';
 
 class UserLegacyController {
-  private path = "/users/legacy";
+	private path = '/users/legacy';
 
-  private router = Router();
+	private router = Router();
 
-  constructor() {
-    this.intializeRoutes();
-  }
+	constructor() {
+		this.intializeRoutes();
+	}
 
-  private intializeRoutes() {
-    this.router.get(`${this.path}`, authMiddleware, this.getAllUsersLegacy);
-    this.router.get(`${this.path}/:id`, this.getUserLegacyById);
-    this.router.delete(
-      `${this.path}/deleteByEmail`,
-      authMiddleware,
-      this.deleteUserByEmail
-    );
-    this.router.patch(`${this.path}`, authMiddleware, this.setUserLegacy);
-  }
+	private intializeRoutes() {
+		this.router.get(`${this.path}`, authMiddleware, this.getAllUsersLegacy);
+		this.router.get(`${this.path}/:id`, this.getUserLegacyById);
+		this.router.delete(
+			`${this.path}/deleteByEmail`,
+			authMiddleware,
+			this.deleteUserByEmail,
+		);
+		this.router.patch(`${this.path}`, authMiddleware, this.setUserLegacy);
+	}
 
-  private getUserLegacyById = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const user = await UserLegacy.findOne({ where: { id: req.params.id } });
-      res.send(user);
-    } catch (error) {
-      sequelizeErrorMiddleware(error, req, res, next);
-    }
-  };
+	private getUserLegacyById = async (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => {
+		try {
+			const user = await UserLegacy.findOne({ where: { id: req.params.id } });
+			res.send(user);
+		} catch (error) {
+			sequelizeErrorMiddleware(error, req, res, next);
+		}
+	};
 
-  private getAllUsersLegacy = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const users = await UserLegacy.findAll({
-        attributes: [
-          "id",
-          "email",
-          "emailConfirmed",
-          "role",
-          "userBanStatus",
-          "provider"
-        ],
-        include: [
-          {
-            model: UserProfileLegacy,
-            as: "profile"
-          }
-        ]
-      });
-      res.send(users);
-    } catch (error) {
-      sequelizeErrorMiddleware(error, req, res, next);
-    }
-  };
+	private getAllUsersLegacy = async (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => {
+		try {
+			const users = await UserLegacy.findAll({
+				attributes: [
+					'id',
+					'email',
+					'emailConfirmed',
+					'role',
+					'userBanStatus',
+					'provider',
+				],
+				include: [
+					{
+						model: UserProfileLegacy,
+						as: 'profile',
+					},
+				],
+			});
+			res.send(users);
+		} catch (error) {
+			sequelizeErrorMiddleware(error, req, res, next);
+		}
+	};
 
-  private deleteUserByEmail = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const email = req.query.email;
-      const user = await UserLegacy.findOne({ where: { email: email } });
-      if (!user) next(new httpException(400, "User does not exist!"));
-      else
-        try {
-          await UserLegacy.destroy({ where: { id: user.id } });
-          next(new httpException(200, "User deleted successful!"));
-        } catch (error) {
-          errorMiddleware(error, req, res, next);
-        }
-    } catch (error) {
-      sequelizeErrorMiddleware(error, req, res, next);
-    }
-  };
+	private deleteUserByEmail = async (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => {
+		try {
+			const email = req.query.email;
+			const user = await UserLegacy.findOne({ where: { email: email } });
+			if (!user) next(new httpException(400, 'User does not exist!'));
+			else
+				try {
+					await UserLegacy.destroy({ where: { id: user.id } });
+					next(new httpException(200, 'User deleted successful!'));
+				} catch (error) {
+					errorMiddleware(error, req, res, next);
+				}
+		} catch (error) {
+			sequelizeErrorMiddleware(error, req, res, next);
+		}
+	};
 
-  private setUserLegacy = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    const data = req.body;
-    try {
-      const user = await UserLegacy.update(data, {
-        where: { id: req.query.params.id }
-      });
-      res.send(user);
-    } catch (error) {
-      sequelizeErrorMiddleware(error, req, res, next);
-    }
-  };
+	private setUserLegacy = async (
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	) => {
+		const data = req.body;
+		delete data.id;
+		try {
+			const user = await UserLegacy.findOne({ where: { id: req.query.id } });
+			if (!user) next(new httpException(400, 'User does not exist!'));
+			else
+				try {
+					await UserLegacy.update(data, {
+						where: { id: req.query.id },
+					});
+					next(new httpException(200, 'User updated successful!'));
+				} catch (error) {
+					errorMiddleware(error, req, res, next);
+				}
+		} catch (error) {
+			sequelizeErrorMiddleware(error, req, res, next);
+		}
+	};
 }
 
 export default UserLegacyController;
--- src/models/userLegacy.model.ts
+++ src/models/userLegacy.model.ts
@@ -1,78 +1,82 @@
 import {
-  Table,
-  Column,
-  Model,
-  CreatedAt,
-  UpdatedAt,
-  IsEmail,
-  IsUUID,
-  PrimaryKey,
-  Default,
-  BeforeCreate,
-  AllowNull,
-  HasOne
-} from "sequelize-typescript";
+	Table,
+	Column,
+	Model,
+	CreatedAt,
+	UpdatedAt,
+	IsEmail,
+	IsUUID,
+	PrimaryKey,
+	Default,
+	BeforeCreate,
+	AllowNull,
+	HasOne,
+	DataType,
+} from 'sequelize-typescript';
 
-import bcryptjs from "bcryptjs";
+import bcryptjs from 'bcryptjs';
 
-import uuidV4 from "uuid/v4";
-import { UserProfileLegacy } from "./";
+import uuidV4 from 'uuid/v4';
+import { UserProfileLegacy } from './';
 
 @Table({
-  tableName: "User"
+	tableName: 'User',
 })
 export class UserLegacy extends Model<UserLegacy> {
-  @IsUUID(4)
-  @PrimaryKey
-  @AllowNull(false)
-  @Column
-  id!: string;
+	@IsUUID(4)
+	@PrimaryKey
+	@AllowNull(false)
+	@Column
+	id!: string;
 
-  @IsEmail
-  @AllowNull(false)
-  @Column
-  email!: string;
+	@IsEmail
+	@AllowNull(false)
+	@Column
+	email!: string;
 
-  @AllowNull(false)
-  @Column
-  password!: string;
+	@AllowNull(false)
+	@Column
+	password!: string;
 
-  @Default(0)
-  @Column
-  emailConfirmed?: number;
+	@Default(0)
+	@Column
+	emailConfirmed?: number;
 
-  @Column
-  type?: string;
+	@Column
+	type?: string;
 
-  @CreatedAt
-  @Column
-  createdAt?: Date;
+	@CreatedAt
+	@Column
+	createdAt?: Date;
 
-  @UpdatedAt
-  @Column
-  updatedAt?: Date;
+	@UpdatedAt
+	@Column
+	updatedAt?: Date;
 
-  @Default(0)
-  @Column
-  userBanStatus?: number;
+	@Default(0)
+	@Column
+	userBanStatus?: number;
 
-  @Default("user")
-  @Column
-  role?: string;
+	@Default('user')
+	@Column
+	role?: string;
 
-  @HasOne(() => UserProfileLegacy)
-  profile: UserProfileLegacy | undefined;
+	@Column(DataType.ENUM('spacenow', 'wework'))
+	provider?: string;
 
-  @BeforeCreate
-  static async generateId(instance: UserLegacy) {
-    instance.id = uuidV4();
-  }
+	@HasOne(() => UserProfileLegacy)
+	profile: UserProfileLegacy | undefined;
 
-  @BeforeCreate
-  static async hashPassword(instance: UserLegacy) {
-    instance.password = bcryptjs.hashSync(
-      instance.password,
-      bcryptjs.genSaltSync(8)
-    );
-  }
+	@BeforeCreate
+	static async generateId(instance: UserLegacy) {
+		instance.id = uuidV4();
+	}
+
+	@BeforeCreate
+	static async hashPassword(instance: UserLegacy) {
+		instance.password = bcryptjs.hashSync(
+			instance.password,
+			bcryptjs.genSaltSync(8),
+		);
+	}
 }
