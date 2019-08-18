ba23560a44026168d111ad5de793772782088856
Arthemus <arthemus.moreira@gmail.com>
2019-08-19 09:54:55 +1000

    Adding passport dependency.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+export const url = process.env.WEBSITE_URL;
+
 export const DEBUG = process.env.DEBUG ? /true/i.test(process.env.DEBUG) : false;
 
 export const subDomain = process.env.SUB_DOMAIN;
@@ -18,3 +20,27 @@ export const payment = {
     secretKey: process.env.STRIPE_SECRET_KEY || '#STRIPE_SECRET_KEY#'
   }
 }
+
+/**
+ * Google: https://cloud.google.com/console/project
+ * Facebook: https://developers.facebook.com
+ */
+export const auth = {
+  jwt: {
+    secret: process.env.JWT_SECRET
+  },
+  redirectURL: {
+    login: '/',
+    verification: '/',
+  },
+  facebook: {
+    id: process.env.FACEBOOK_APP_ID,
+    secret: process.env.FACEBOOK_APP_SECRET,
+    returnURL: `${url}/login/facebook/return`,
+  },
+  google: {
+    id: process.env.GOOGLE_CLIENT_ID,
+    secret: process.env.GOOGLE_CLIENT_SECRET,
+    returnURL: `${url}/login/google/return`,
+  }
+};
