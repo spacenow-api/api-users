c5d5d0564a3b8714beced6c19b12f6299dad9cbd
Arthemus <arthemus.moreira@gmail.com>
2019-08-26 12:58:18 +1000

    Simplifying facebook login.

--- src/helpers/oauth/facebook.ts
+++ src/helpers/oauth/facebook.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Strategy } from 'passport-facebook';
+import PassportFacebookToken from 'passport-facebook-token';
 import passport from 'passport';
 
 import sequelizeErrorMiddleware from '../middlewares/sequelize-error-middleware';
@@ -15,45 +16,35 @@ import { auth, subDomain } from "../../config";
 
 class FacebookOAuthStrategy {
 
-  public static RETURN_MIDDLEWARE = passport.authenticate('facebook', { failureRedirect: '/login', session: false });
+  public static RETURN_MIDDLEWARE = passport.authenticate('facebook-token', { failureRedirect: '/login', session: false });
 
   public static initialize() {
-    passport.use(new Strategy({
+    passport.use(new PassportFacebookToken({
       clientID: auth.facebook.id,
       clientSecret: auth.facebook.secret,
-      callbackURL: `${auth.facebook.returnURL}`,
-      profileFields: ['name', 'email', 'birthday', 'link', 'locale', 'timezone', 'picture.width(255).height(255)'],
-      passReqToCallback: true
-    }, (req: Request, accessToken: any, refreshToken: any, profile: any, done: any) => {
-      const _ = async () => {
-        if (req.user) {
-          await UserVerifiedInfoLegacy.update({ isFacebookConnected: true }, { where: { userId: req.user.id } });
-          done(null, { type: 'verification' });
+      profileFields: ['name', 'email', 'birthday', 'link', 'locale', 'timezone', 'picture.width(255).height(255)']
+    }, async (accessToken: any, refreshToken: any, profile: any, done: any) => {
+      const email = (profile.emails && profile.emails.length > 0) ? profile.emails[0].value : profile.email;
+      const userData = await UserLegacy.findOne({ where: { email }, attributes: ['id', 'email', 'userBanStatus'] });
+      if (userData) {
+        if (userData.userBanStatus == 1) {
+          return done(null, { id: userData.id, email: userData.email, type: 'userbanned' });
         } else {
-          const email = (profile.emails && profile.emails.length > 0) ? profile.emails[0].value : profile.email;
-          const userData = await UserLegacy.findOne({ where: { email }, attributes: ['id', 'email', 'userBanStatus'] });
-          if (userData) {
-            if (userData.userBanStatus == 1) {
-              return done(null, { id: userData.id, email: userData.email, type: 'userbanned' });
-            } else {
-              // There is an account associated with this email...
-              await UserVerifiedInfoLegacy.update({ isFacebookConnected: true }, { where: { userId: userData.id } });
-              return done(null, { id: userData.id, email: userData.email, type: 'login' });
-            }
-          } else {
-            const authService = new AuthenticationService();
-            const { _json: gObj } = profile;
-            const userObj = <IUserLegacySignUpRequest>{
-              email: gObj.email,
-              firstName: gObj.given_name,
-              lastName: gObj.family_name
-            };
-            const userCreated = await authService.registerNewUser(userObj);
-            return done(null, { id: userCreated.id, email: userCreated.email, type: 'login' });
-          }
+          // There is an account associated with this email...
+          await UserVerifiedInfoLegacy.update({ isFacebookConnected: true }, { where: { userId: userData.id } });
+          return done(null, { id: userData.id, email: userData.email, type: 'login' });
         }
-      };
-      _().catch(done);
+      } else {
+        const authService = new AuthenticationService();
+        const { _json: gObj } = profile;
+        const userObj = <IUserLegacySignUpRequest>{
+          email: gObj.email,
+          firstName: gObj.given_name,
+          lastName: gObj.family_name
+        };
+        const userCreated = await authService.registerNewUser(userObj);
+        return done(null, { id: userCreated.id, email: userCreated.email, type: 'login' });
+      }
     }));
     return new FacebookOAuthStrategy();
   }
